97abb5878f9eb8dfbd949f45d500a41f9e91ea8b
ajolafsson <amanda.olafsson@gmail.com>
2014-07-25 22:23:31 -0400

    committing Programming Assignment 2 for Homework

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The goal of these functions are to store calculations that could become
+## time consuming to reduce processing time.
 
-## Write a short comment describing this function
+## This function creates an object that stores the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function (y){
+      x<<-y
+      m<<-NULL
+    }
+    get<-function() x
+    setmatrix<-function(solve) m<<- solve
+    getmatrix<-function() m
+    list (set=set, get=get, setmatrix=setmatrix,getmatrix=getmatrix)
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix in the function above
+## but only does it if the inverse hasn't already been calculated.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m<-x$getmatrix()
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  }
+  matrix<-x$get ()
+  m<-solve(matrix, ...)
+  x$setmatrix(m)
+  m     
+  
+  ## Return a matrix that is the inverse of 'x'
 }
+
+
+### testing
+a <- seq(1:9)
+r <- matrix(a, 3, 3, byrow = TRUE)
+c <- matrix(a, 3, 3, byrow = FALSE)
